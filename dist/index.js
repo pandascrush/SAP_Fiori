b7199834dea83e575555b6379ea3814fbed45293
pandascrush <velayuthamsiva55@gmail.com>
2024-08-09 18:30:27 +0530

    new commit

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,9 @@
-sap.ui.define(["sap/ui/core/mvc/XMLView"],function(e){"use strict";e.create({viewName:"sap.ui.demo.walkthrough.view.View1"}).then(function(e){e.placeAt("content")})});
+sap.ui.define(["sap/ui/core/mvc/XMLView"], function (e) {
+  "use strict";
+  e.create({ viewName: "sap.ui.demo.walkthrough.view.View1" }).then(function (
+    e
+  ) {
+    e.placeAt("content");
+  });
+});
 //# sourceMappingURL=index.js.map
